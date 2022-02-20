5aa0c563204131e55e5be5a83833b54010e7571d
Mahmoodur Rahman <mahmoodnnb@gmail.com>
2022-02-19 19:31:11 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import altair as alt
 from vega_datasets import data
 import pandas as pd
 import numpy as np
-​
+
 url = "https://raw.githubusercontent.com/UofTCoders/workshops-dc-py/master/data/processed/world-data-gapminder.csv"
 df = pd.read_csv(url, parse_dates = ['year'])
-​
-​
+
 def plot_chart(year= "1962", df=df.copy()):
     # Filter data for a year
     df = df.query(f'year == {year}')
-​
+
     chart = alt.Chart(df).mark_circle().encode(
     alt.X('children_per_woman', scale=alt.Scale(zero=False)),
     alt.Y('life_expectancy',
@@ -27,12 +26,11 @@ def plot_chart(year= "1962", df=df.copy()):
     fontSize=30
 )
     return chart.to_html()
-​
-​
+
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
-​
+
 app.layout = html.Div(
     [
     html.H1('LifeExpectancy Plot of Gapminder', style={'color': 'blue', 'fontSize': 30}),
@@ -55,28 +53,14 @@ app.layout = html.Div(
         ),
     ]
 )
-​
-​
+
 @app.callback(
     Output("iframe", "srcDoc"),
     Input("year", "value")
 )
-​
+
 def update_output(year):
     return plot_chart(year)
-​
+
 if __name__ == '__main__':
     app.run_server(debug=True)
-Collapse
-
-
-
-
-
-
-
-
-
-
-
-
